4e412c9ceb8ec1e3b4495ae4bdeb0413a21e8b3b
dev <dev@zlab.dev>
2021-08-19 11:17:19 +0800

    define & redis handle

---
+++ README.md
@@ -0,0 +1,18 @@
+## api login response
+
+```json
+{
+    "uid": "1234567890",
+    "token": "27666b82-a171-40dc-b33f-ca72d4028837",
+    "key": "48ad4d03abf9890e496a2c38d4419c7a"
+}
+```
+
+## auth
+
+```json
+{
+    "uid": "1234567890",
+    "token": "27666b82-a171-40dc-b33f-ca72d4028837"
+}
+```
---
+++ define.go
@@ -0,0 +1,19 @@
+package zlabws
+
+const (
+    NullType uint8 = 0x00
+    AuthType uint8 = 0x01
+)
+
+const (
+    CustomType   uint8 = 0x10
+    TextType     uint8 = 0x11
+    ImageType    uint8 = 0x12
+    VoiceType    uint8 = 0x13
+    VideoType    uint8 = 0x14
+    FileType     uint8 = 0x15
+    StickerType  uint8 = 0x16
+    LocationType uint8 = 0x17
+    NoticeType   uint8 = 0x18
+    CommandType  uint8 = 0x40
+)
