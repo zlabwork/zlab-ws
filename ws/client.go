32a9981afdeb0dcd84d1020b94d34aa7a2dc815e
Joe <joe@xxtime.com>
2021-08-22 16:37:54 +0800

    跨域

--- ws/client.go
+++ ws/client.go
@@ -41,6 +41,10 @@ var (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	// 跨域
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 // Client is a middleman between the websocket connection and the hub.
