c8b3305f59af11dc84daed0f82b6014bcbbe2d75
dev <dev@zlab.dev>
2022-03-11 11:27:32 +0800

    Libs - Snowflake

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module app
 go 1.14
 
 require (
+	github.com/bwmarrin/snowflake v0.3.0
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/uuid v1.3.0
--- libs.go
+++ libs.go
@@ -1,10 +1,22 @@
 package app
 
+import (
+	"github.com/bwmarrin/snowflake"
+	"os"
+	"strconv"
+)
+
 var Libs *libs
 
 type libs struct {
+	Snow *snowflake.Node
 }
 
 func NewLibs() *libs {
-	return &libs{}
+	i, _ := strconv.ParseInt(os.Getenv("APP_NODE"), 10, 64)
+	snowflake.Epoch = 1498612200000
+	node, _ := snowflake.NewNode(i)
+	return &libs{
+		Snow: node,
+	}
 }
