946ec3f16c140cb9c7539c8baf4441df82042a27
dev <dev@zlab.dev>
2022-04-21 17:12:46 +0800

    Schema and Docs

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： 127.0.0.1
--- 生成日期： 2022-03-22 01:50:42
+-- 生成日期： 2022-04-21 09:11:30
 -- 服务器版本： 10.2.7-MariaDB
 -- PHP 版本： 8.0.9
 
@@ -45,12 +45,25 @@ CREATE TABLE `im_logs` (
 
 CREATE TABLE `im_msg` (
   `id` int(10) UNSIGNED NOT NULL,
-  `mid` varchar(36) NOT NULL DEFAULT '',
-  `type` tinyint(4) UNSIGNED NOT NULL DEFAULT 0,
-  `sender` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
-  `receiver` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
-  `data` varchar(2048) NOT NULL DEFAULT '',
+  `sid` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '会话ID',
+  `seq` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户序列',
+  `send` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '发布用户',
+  `data` varbinary(4096) NOT NULL,
   `ctime` datetime NOT NULL DEFAULT '0001-01-01 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `im_session`
+--
+
+CREATE TABLE `im_session` (
+  `id` bigint(20) UNSIGNED NOT NULL COMMENT '会话ID',
+  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
+  `uids` varchar(5000) NOT NULL DEFAULT '' COMMENT '参与用户',
+  `ctime` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `mtime` datetime NOT NULL DEFAULT '0001-01-01 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -61,11 +74,10 @@ CREATE TABLE `im_msg` (
 
 CREATE TABLE `im_todo` (
   `id` int(10) UNSIGNED NOT NULL,
-  `mid` varchar(36) NOT NULL DEFAULT '',
-  `type` tinyint(4) UNSIGNED NOT NULL DEFAULT 0,
-  `sender` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
-  `receiver` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
-  `data` varchar(2048) NOT NULL DEFAULT '',
+  `sid` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '会话ID',
+  `seq` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户序列',
+  `send` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '发布用户',
+  `data` varbinary(4096) NOT NULL,
   `ctime` datetime NOT NULL DEFAULT '0001-01-01 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -86,10 +98,17 @@ ALTER TABLE `im_msg`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 表的索引 `im_session`
+--
+ALTER TABLE `im_session`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 表的索引 `im_todo`
 --
 ALTER TABLE `im_todo`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `send` (`send`);
 
 --
 -- 在导出的表使用AUTO_INCREMENT
